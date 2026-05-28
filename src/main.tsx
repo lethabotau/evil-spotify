b19e068103ae5a66e12a3e08e453a4ded5a21127
lethabotau <lethabojadentau@gmail.com>
2026-05-29 06:30:04 +1000

    deploy

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { CorruptionProvider } from './context/CorruptionContext'
 import './global.css'
 import './index.css'
@@ -9,9 +9,9 @@ import App from './App.tsx'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <CorruptionProvider>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </CorruptionProvider>
   </StrictMode>,
 )
